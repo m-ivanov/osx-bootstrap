5a225f099f065b39edb39fd4360e4642d6075fec
m-ivanov <m.ivanov.reply@gmail.com>
2017-08-12 22:07:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 dir="$HOME/tmp"
 mkdir -p $dir
 cd $dir
-git clone --recursive https://github.com/iem4voos/osx-bootstrap.git
+git clone --recursive https://github.com/m-ivanov/osx-bootstrap.git
 cd osx-bootstrap
 bash bootstrap.sh
